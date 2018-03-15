2fd1b27ebd93afc0db39e198463e4e317c74de92
Tony Tung <ttung@chanzuckerberg.com>
2018-03-15 10:08:15 -0700

    -> rst

--- setup.py
+++ setup.py
@@ -15,6 +15,6 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     install_requires=install_requires,
     entry_points={
-        'console_scripts': "imageslice=imageslice:imageslice"
+        'console_scripts': "imageslice=imageslice:cli"
     }
 )
