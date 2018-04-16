1a8a4e0a0e7b1f3673b4cbe9599e91694d988e51
Tony Tung <ttung@chanzuckerberg.com>
2018-04-15 23:01:15 -0400

    0.0.0-dev2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ install_requires = [line.rstrip() for line in open(os.path.join(os.path.dirname(
 
 setuptools.setup(
     name="slicedimage",
-    version="0.0.0-dev1",
+    version="0.0.0-dev2",
     description="Library to access sliced imaging data",
     author="Tony Tung",
     author_email="ttung@chanzuckerberg.com",
